12ba7673af95f879fc971200656a2df150d8e480
Eshaan SN <eshaansn@gmail.com>
2022-10-25 22:00:20 +0530

    Added Buttons for better user experience

--- sketch.js
+++ sketch.js
@@ -1,17 +1,29 @@
 let cluster;
+let step;
+let reset;
+
 function setup() {
   createCanvas(600, 400);
-  cluster = new Cluster(100, 3);
-  cluster.Simulate();
-  // cluster.simulateRandom();
-  cluster.Initialize();
-  cluster.Analyse()
-  cluster.Show()
+  resetFunction()
+
+  step = createButton('Step')
+  step.mousePressed(stepFunction)
+
+  reset = createButton('Reset')
+  reset.mousePressed(resetFunction)
 }
 
-function mousePressed() {
+function stepFunction() {
   background(0);
   cluster.Step()
   cluster.Show();
+}
+
+function resetFunction(){
+  cluster = new Cluster(100, 3);
+  cluster.Simulate();
+  cluster.Initialize();
+  cluster.Analyse()
+  cluster.Show()
 }
 
